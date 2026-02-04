d21a621202efe0e3ffe70fd3524496282366e404
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-04 16:12:44 +0530

    make state graph for node can read/write

---
+++ backend/graph/state.py
@@ -0,0 +1,15 @@
+from typing import TypedDict
+
+class ProjectState(TypedDict):
+    idea:str
+    experience: str
+    time: str
+    team: int
+    tech: str
+
+    scope_risk:str
+    time_risk:str
+    skill_risk:str
+    tech_risk:str
+
+    final_analysis:str
