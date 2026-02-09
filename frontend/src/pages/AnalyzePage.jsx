54150ca5511540b9d236a80adc3ffb5d6fabcf5b
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-09 13:41:33 +0530

    updated spinner file, style sheet, analyze page

--- frontend/src/pages/AnalyzePage.jsx
+++ frontend/src/pages/AnalyzePage.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { analyzeProject } from "../api";
 import Spinner from "../components/Spinner"
+import "../styles.css"
 
 const AnalyzePage = () => {
   const navigate = useNavigate();
