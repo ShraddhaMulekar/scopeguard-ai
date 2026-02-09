e200ef2c2cb8f52c655e223aa657e38a4b7e630c
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-09 12:33:19 +0530

    install dependency and write AnalyzePage logic

--- frontend/src/pages/AnalyzePage.jsx
+++ frontend/src/pages/AnalyzePage.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AnalyzePage = () => {
-  return (
-    <div>AnalyzePage</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default AnalyzePage
+  const [form, setForm] = useState({
+    idea: "",
+    experience: "",
+    time_weeks: "",
+    team: "",
+    tech: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  return (
+    <div>
+      <h2>Project Risk Analyzer</h2>
+
+      <form action="">
+        <input
+          type="text"
+          name="idea"
+          placeholder="Project Idea"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="experience"
+          placeholder="Experience Level Ex.(beginner, medium, advance)"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="time_weeks"
+          placeholder="Time (weeks)"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="team"
+          placeholder="Team size"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="tech"
+          placeholder="Tech Stack"
+          required
+          onChange={handleChange}
+        />
+        <button type="submit" disabled={loading}>
+          {loading ? "Analyzing..." : "Analyze"}
+        </button>
+      </form>
+      
+      {loading && <Spinner />}
+      {error && <p className="error">{error}</p>}
+    </div>
+  );
+};
+
+export default AnalyzePage;
