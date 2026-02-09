c3b0db165241864a8e4ad73d39ccfa0f89855f64
ShraddhaMulekar <mulekarshraddha@gmail.com>
2026-02-09 20:59:50 +0530

    add frontend deployed vercel link

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="ScopeGuard AI")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://scopeguard-ai-eight.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
